00204b4719969276454970a7642190974e9275b3
Ebod Shojaei <ebod.shojaei@gmail.com>
2023-10-31 12:32:52 -0700

    Error handling for invalid inputs

--- calculator.c
+++ calculator.c
@@ -1,8 +1,40 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+/**
+ * Clears user inputs.
+ * @see https://stackoverflow.com/questions/69997896/clearing-incorrect-scanf-inputs-in-c 
+ */
+void clearInput() {
+	int c;
+	while((c = getchar()) != '\n' && c != EOF);
+}
+
 int add(int operand_a, int operand_b) {
 	return operand_a + operand_b;
+}
+
+void getNum(int *operand_a, int *operand_b) {
+	// Prompt user to enter two operands.
+	printf("Enter a number (0 - 9999)\n");
+	int input_a = scanf("%d", operand_a);
+
+	// Error handle invalid operand.
+	while (input_a != 1 || abs(*operand_a) > 9999) {
+		printf("Invalid number.\nEnter a number (0 - 9999).\n");
+		clearInput();
+		input_a = scanf("%d", operand_a);
+	}
+	
+	printf("Enter another number (0 - 9999)\n");
+	int input_b = scanf("%d", operand_b);
+
+	// Error handle invalid operand.
+	while (input_b != 1 || (*operand_b) > 9999) {
+		printf("Invalid number.\nEnter another number (0 - 9999).\n");
+		clearInput();
+		input_b = scanf("%d", operand_b);
+	}
 }
 
 int main() {
@@ -10,37 +42,38 @@ int main() {
 	int operand_b;
 	int operator;
 	int result;
+	
+	// Welcome the user.
+	printf("\n\n ******************\n Welcome to Calculy\n ******************\n\n");
 
 	// Enable looping for repeated computations.	
 	while (operator != 5) {
-		printf("\n ******************\n Welcome to Calculy\n ******************\n\n");
-
-		// Prompt user to enter two operands.
-		printf("Enter a number (0 - 9999)\n");
-		scanf("%d", &operand_a);
-		printf("Enter another number (0 - 9999)\n");
-		scanf("%d", &operand_b);
-		printf("You entered: %d, %d\n", operand_a, operand_b);
-	
 		// Prompt user to select an operation.
 		printf("Select an operation:\n[1] ADD\n[2] SUBTRACT\n[3] MULTIPLY\n[4] DIVIDE\n[5] EXIT\n");
-		scanf("%d", &operator);
+	
+		// Error handle non-int input.	
+		int choice = scanf("%d", &operator);
+		if (choice != 1) {
+			printf("Invalid option selected.\n\n");
+			clearInput();
+			continue;
+		}
 
-		result = 0; // Zero-out result for next operation.
 		switch (operator) {
 			case 1:
-				int result = add(operand_a, operand_b);
-				printf("The sum of %d + %d = %d\n", operand_a, operand_b, result);
+				getNum(&operand_a, &operand_b);
+				result = add(operand_a, operand_b);
+				printf("The sum of %d + %d = %d\n\n", operand_a, operand_b, result);
 				break;
 			case 5:
 				printf("Thank you for calculating.\n");
-				return 0;		
+				break;
+			default:
+				// Error handle invalid int input ( > 5 || < 1 ).
+				printf("Invalid option selected.\n\n");
+				clearInput();
 		}
 	}
-
-
-
-
 
 	return 0;
 }
